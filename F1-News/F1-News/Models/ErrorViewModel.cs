6c51b39a02e5b3cdbba1fe97b7cb3ac64e59e09b
Fabian Steinlechner <fasteinlechner@tsn.at>
2021-11-04 10:52:54 +0100

    First Config

--- F1-News/F1-News/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace F1_News.Models {
-    public class ErrorViewModel {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
