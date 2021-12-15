11bedefe5cee23a9a2796a4ecb9b5e5398a8a7b3
dl46 <85621605+dl46@users.noreply.github.com>
2021-12-15 22:11:19 +0100

    Forms

--- F1-News/F1-News/Controllers/UserController.cs
+++ F1-News/F1-News/Controllers/UserController.cs
@@ -16,7 +16,12 @@ namespace F1_News.Controllers {
             return View();
         }
 
-       [HttpPost]
+        [HttpGet]
+        public IActionResult Login() {
+            return View();
+        }
+
+        [HttpPost]
         public IActionResult Registration(User userDataFromForm) {
            
             if (userDataFromForm == null) {
@@ -35,8 +40,69 @@ namespace F1_News.Controllers {
                 return;
             }
            
+            //Username
             if(user.Username==null || user.Username.Trim().Length < 4) {
                 ModelState.AddModelError("Username", "Der Username muss mind. 4 Zeichen lang sein");
+            }
+
+            //Password
+            if ((user.Password == null) || (user.Password.Length < 8))
+            {
+                ModelState.AddModelError("Password", "Das Passwort muss mindestens 8 Zeichen lang sein!");
+            }
+
+            //Gender
+            if (user.Gender.Equals(null))
+            {
+                ModelState.AddModelError("Gender", "Sie müssen ein Geschlecht angeben!");
+            }
+
+            //Birthdate
+            if (user.Birthdate > DateTime.Now)
+            {
+                ModelState.AddModelError("Birthdate", "Das Geburtsdatum darf nicht in der Zukunft sein!");
+            }
+
+            /*EMail
+            if (!user.Email.Contains("@"))
+            {
+                ModelState.AddModelError("EMail", "Geben Sie eine gültige E-Mail-Adresse an!");
+            }*/
+
+        }
+
+        [HttpPost]
+        public IActionResult Login(User userDataFromForm) {
+
+            if (userDataFromForm == null)
+            {
+                return RedirectToAction("Login");
+            }
+            ValidateLoginData(userDataFromForm);
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(userDataFromForm);
+
+        }
+
+        private void ValidateLoginData(User user) {
+            if (user == null)
+            {
+                return;
+            }
+
+            //Username
+            if (user.Username == null || user.Username.Trim().Length < 4)
+            {
+                ModelState.AddModelError("Username", "Der Username muss mind. 4 Zeichen lang sein");
+            }
+
+            //Password
+            if ((user.Password == null) || (user.Password.Length < 8))
+            {
+                ModelState.AddModelError("Password", "Das Passwort muss mindestens 8 Zeichen lang sein!");
             }
 
         }
